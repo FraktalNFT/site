d1eeb085cf8e6d588ec0370138cb25a6b0e9054c
mkdir <projectdevelopinginbox@gmail.com>
2021-07-06 15:22:05 -0500

    join us link

--- components/Values.tsx
+++ components/Values.tsx
@@ -9,7 +9,7 @@ const Values = () => {
         <p>Fraktal is built on Ethereum because it embodies the values of inclusivity, transparency, and trust. The network is owned by its users, open to all, and free from censorship.</p>
       </div>
       <div className='join'>
-        <h3><b>JOIN US</b> AND SHAPE THE FUTURE OF ART</h3>
+        <h3><Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><a><b>JOIN US</b></a></Link>AND SHAPE THE FUTURE OF ART</h3>
         <div>
           <Link href='https://twitter.com/FraktalNFT'><a><Image src='/Twitter/Original.svg' width={37} height={37} alt='' /></a></Link>
           <Link href='https://www.instagram.com/fraktal.io/'><a><Image src='/Instagram/Original.svg' width={37} height={37} alt='' /></a></Link>
