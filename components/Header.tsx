e6eb3e92430c98e94863a3e7c8f2fe7986d3891c
mkdir <projectdevelopinginbox@gmail.com>
2021-07-07 14:46:18 -0500

    add hamburger navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from 'react';
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  function showNav() {
+    const hamburgerNav = document.querySelector('.hamburger-nav');
+    (!open) ? hamburgerNav.classList.remove('hide') : hamburgerNav.classList.add('hide');
+    setOpen(!open);
+  }
   return (
     <header>
       <div className='logo-small'>
@@ -9,13 +16,21 @@ const Header = () => {
           <a><Image src='/fraktal-logo-small.svg' width={75} height={50} alt='' /></a>
         </Link>
       </div>
-      <nav>
+      <div onClick={showNav} className='hamburger'>
+        <Image src='/hamburger.svg' width={45} height={30} alt='' />
+      </div>
+      <nav className='main-nav'>
         <Link href='https://twitter.com/FraktalNFT'><a><Image src='/Twitter/Original.svg' width={25} height={25} alt='' /></a></Link>
         <Link href='https://www.instagram.com/fraktal.io/'><a><Image src='/Instagram/Original.svg' width={25} height={25} alt='' /></a></Link>
         <Link href='https://discord.gg/Bs56GyMH'><a><Image src='/Discord/Original.svg' width={25} height={25} alt='' /></a></Link>
         <Link href='https://medium.com/@fraktal'><a className='link-text'>BLOG</a></Link>
         <Link href='https://dao.fraktal.io'><a className='link-text'>DAO</a></Link>
         <Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><button>Enter Fraktal</button></Link>
+      </nav>
+      <nav className='hamburger-nav hide'>
+        <Link href='https://medium.com/@fraktal'><a className='link-text'>BLOG</a></Link>
+        <Link href='https://dao.fraktal.io'><a className='link-text'>DAO</a></Link>
+        <Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><a className='link-text'>Enter Fraktal</a></Link>        
       </nav>
     </header>
   );
