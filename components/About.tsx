15bf5e411baef67016cacb360bb92df6dc62b2b8
mkdir <projectdevelopinginbox@gmail.com>
2021-07-06 21:35:09 -0500

    fix text alignment

--- components/About.tsx
+++ components/About.tsx
@@ -11,17 +11,17 @@ const About = () => {
         <div>
           <Image src='/access-icon.svg' width={220} height={220} alt='' />
           <h4>Access for all</h4>
-          <p>Fraktions grant access to the NFT without the often unaffordable price tag.</p>
+          <p className='access-text'>Fraktions grant access to the NFT without the often unaffordable price tag.</p>
         </div>
         <div>
           <Image src='/support-icon.svg' width={220} height={220} alt='' />
           <h4>Support artists, not gatekeepers</h4>
-          <p>What you pay for a Fraktion goes directly to the creator, not an auction house.</p>
+          <p className='support-text'>What you pay for a Fraktion goes directly to the creator, not an auction house.</p>
         </div>
         <div>
           <Image src='/union.svg' width={220} height={220} alt='' />
           <h4>Grow value</h4>
-          <p>As your favorite NFTs mature in value, your collection of Fraktions does too.</p>
+          <p className='grow-text'>As your favorite NFTs mature in value, your collection of Fraktions does too.</p>
         </div>
       </div>
     </section>
