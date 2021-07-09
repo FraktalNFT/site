902a6f286faf4918ef81bd87de3c0533554149b1
mkdir <projectdevelopinginbox@gmail.com>
2021-07-09 09:08:27 -0500

    add social links to dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,7 +28,12 @@ const Header = () => {
       <nav className={`hamburger-nav ${open ? '' : 'hide'}`}>
         <Link href='https://medium.com/@fraktal'><a className='link-text'>BLOG</a></Link>
         <Link href='https://dao.fraktal.io'><a className='link-text'>DAO</a></Link>
-        <Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><a className='link-text'>Enter Fraktal</a></Link>        
+        <Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><a className='link-text'>Enter Fraktal</a></Link>
+        <div>
+          <Link href='https://twitter.com/FraktalNFT'><a><Image src='/Twitter/Original.svg' width={25} height={25} alt='' /></a></Link>
+          <Link href='https://www.instagram.com/fraktal.io/'><a><Image src='/Instagram/Original.svg' width={25} height={25} alt='' /></a></Link>
+          <Link href='https://discord.gg/XSXE3rR9DU'><a><Image src='/Discord/Original.svg' width={25} height={25} alt='' /></a></Link>
+        </div>
       </nav>
     </header>
   );
