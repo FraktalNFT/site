3db48dc63d6e3a53c00db52c6f12910a18cdd551
cryptnotiq <17i@protonmail.ch>
2021-07-12 11:27:46 +0200

    added social icons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,6 +13,14 @@ const Hero = () => {
         <Image src='/single-leaf.svg' width={55} height={55} alt='' />
         <h2>NFTS</h2>
       </div>
+      <div className='join'>
+      
+        <div>
+          <Link href='https://twitter.com/FraktalNFT'><a><Image src='/Twitter/Original.svg' width={37} height={37} alt='' /></a></Link>
+          <Link href='https://www.instagram.com/fraktal.io/'><a><Image src='/Instagram/Original.svg' width={37} height={37} alt='' /></a></Link>
+          <Link href='https://discord.gg/XSXE3rR9DU'><a><Image src='/Discord/Original.svg' width={37} height={37} alt='' /></a></Link>
+        </div>
+        </div>
   {/*    <Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><button>Get Started</button></Link> */}
     </section>
   );
