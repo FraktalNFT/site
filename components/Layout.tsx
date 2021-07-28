a5dc1f3b7e2899beff30907873bd35a5a911118b
cryptnotiq <17i@protonmail.ch>
2021-07-28 19:37:25 +0200

    moved script after meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,21 +5,23 @@ const Layout: React.FC = ({ children }) => {
   return (
     <html>
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-095Q8484JB"></script>
-      <script
-         async
-         dangerouslySetInnerHTML={{
-           __html: `window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
-           gtag('config', 'G-095Q8484JB');`
-         }}
-       />
+
         <title>Fraktal | Fractional NFTs</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="description" content="Fractional NFTs powered by a DAO." />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-095Q8484JB"></script>
+        <script
+           async
+           dangerouslySetInnerHTML={{
+             __html: `window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', 'G-095Q8484JB');`
+           }}
+         />
       </Head>
       {children}
    </html>
