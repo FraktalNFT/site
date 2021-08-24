33c30aaab54565c65ea74d10544a253b5cd8d324
earth2travis <travis.mccutcheon@gmail.com>
2021-08-24 11:02:27 -0500

    Format about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,29 +1,16 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 const About = () => {
   return (
     <section className='about-section'>
       <div className='fraktal-mission'>
-      <br></br>
-      <br></br><br></br>
-      <br></br><br></br>
-
-
-
-        <h2>Unleash the Value of NFTs</h2>
-        <p>For NFT collectors who are bored with hodling, Fraktal DAO unlocks the power of NFTs by enabling community driven investment, curation, and monetisation.</p>
-
-      
-          <br></br>
-        <button>Learn More Button - scrolls down to next section</button>
-
-
-
-
-  </div>
-
-
-
+          <h2>Unleash the Value of NFTs</h2>
+          <p>For NFT collectors who are bored with hodling, Fraktal DAO unlocks the power of NFTs by enabling community driven investment, curation, and monetisation.</p>
+      </div>
+      <div className='about-button'>
+        <button>Learn More</button>
+      </div>
     </section>
   );
 }
