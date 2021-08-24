bd0b04a1bb20f54144ab8c0115f5fa3f5fd3484c
earth2travis <travis.mccutcheon@gmail.com>
2021-08-24 11:43:50 -0500

    Format benefits section

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -4,13 +4,12 @@ const Benefits = () => {
   return (
     <section className='benefits-section'>
       <div className='heading-div'>
-        <h1>Go Further Together</h1>
-        <h2>Collect, manage and grow together as a community of top collectors working towards disrupting the creative industry to put power back in the hands of artists and individuals. </h2>
-
+        <h2>Go Further Together</h2>
+        <p>Collect, manage and grow together as a community of top collectors working towards disrupting the creative industry to put power back in the hands of artists and individuals.</p>
       </div>
       <div className='benefits-icon'>
         <Image src='/engage-icon.svg' width={180} height={180} alt='' />
-        <p>Invest alongside the top collectors in the industry.</p>
+        <p>Invest alongside the top collectors in the industry</p>
       </div>
       <div className='benefits-icon'>
         <Image src='/monetize-icon.svg' width={180} height={180} alt='' />
@@ -18,7 +17,7 @@ const Benefits = () => {
       </div>
       <div className='benefits-icon'>
         <Image src='/retain-icon.svg' width={180} height={180} alt='' />
-        <p>Experience the metaverse like never before through DAO Galleries.</p>
+        <p>Experience the metaverse like never before through DAO Galleries</p>
       </div>
       <div className='benefits-icon'>
         <Image src='/retain-icon.svg' width={180} height={180} alt='' />
