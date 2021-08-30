dca2553e85a6945c2f62d8e53abc89ca0be56b04
earth2travis <travis.mccutcheon@gmail.com>
2021-08-30 14:56:23 -0500

    Style marketplace section on desktop

--- components/How.tsx
+++ components/How.tsx
@@ -8,18 +8,18 @@ const How = () => {
       <div className="guide-icons">
         <div>
           <Image src="/mint.svg" width={200} height={200} alt="" />
-            <h3>Diverse</h3>
-            <p>The easiest and most secure way to diversify NFT holdings.</p>
+          <h3>Diverse</h3>
+          <p>The easiest and most secure way to diversify NFT holdings.</p>
         </div>
         <div>
           <Image src="/frak.svg" width={200} height={200} alt="" />
-            <h3>Liquid</h3>
-            <p>Leverage fractional NFTs to vastly increase liquidity.</p>
+          <h3>Liquid</h3>
+          <p>Leverage fractional NFTs to vastly increase liquidity.</p>
         </div>
         <div>
           <Image src="/distribute.svg" width={200} height={200} alt="" />
-           <h3>Income Generating</h3>
-            <p>Create revenue streams directly from metaverse galleries.</p>
+          <h3>Income Generating</h3>
+          <p>Create revenue streams directly from metaverse galleries.</p>
         </div>
       </div>
       {/*  <Link href='https://fraktal-front-git-web3-integration-sunguru98.vercel.app/'><button>Try It Out</button></Link> */}
